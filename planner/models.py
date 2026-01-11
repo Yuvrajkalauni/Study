a5904e7c1c5ce27964e42631a71db704bfbb6016
Yuvrajkalauni <yuvrajkalauni8@gmail.com>
2026-01-11 21:27:37 +0545

    done with models

--- planner/models.py
+++ planner/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 class Subject(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -9,19 +10,22 @@ class Subject(models.Model):
 
     def __str__(self):
         return self.name
+
     
 class StudySession(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
-    subject = models.Foreignkey(Subject,on_delete=models.CASCADE)
-    planned_duration = models.Integerfield()
-    actual_duration = models.IntegerField(Default=0)
-    data = models.DateField()
-    complete = models.BooleanField(default=false)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    planned_duration = models.IntegerField()  # minutes
+    actual_duration = models.IntegerField(default=0)
+    date = models.DateField()
+    completed = models.BooleanField(default=False)
 
     def __str__(self):
-        return f"{self.objects.name} = {self.data}"
+        return f"{self.subject.name} - {self.date}"
+
     
 class FocusLog(models.Model):
-    session = models.Foreginkey(StudySession,on_delete=models.CASCADE)
-    start_time = models.DateField()
-    end_time = models.DateField(null=True, blank=False)
+    session = models.ForeignKey(StudySession, on_delete=models.CASCADE)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField(null=True, blank=True)
+
